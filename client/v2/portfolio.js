7019ee560196d8b701de5e84e09e35215360de7a
nlsferrara@gmail.com <100352279+nlsferrara@users.noreply.github.com>
2023-03-01 11:47:13 +0100

    TD2 Clear fashion

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -28,6 +28,11 @@ const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
 const selectBrand = document.querySelector('#brand-select');
 const spanNbBrands = document.querySelector('#nbBrands');
+const selectReleased = document.querySelector('#released-select');
+const selectPrice = document.querySelector('#price-select');
+const selectSort = document.querySelector('#sort-select');
+const spanNbNewProducts = document.querySelector('#nbNewProducts');
+//var selectFavorite = document.querySelector('#cb${counter}');
 
 /**
  * Set global value
@@ -71,13 +76,16 @@ const fetchProducts = async (page = 1, size = 12) => {
 const renderProducts = products => {
   const fragment = document.createDocumentFragment();
   const div = document.createElement('div');
+  var counter = 0;
   const template = products
     .map(product => {
+      counter++;
       return `
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
+        <a href="${product.link}" target="_blank">${product.name}</a>
         <span>${product.price}</span>
+        <input id="cb${counter}" class="star" type="checkbox" title="bookmark page" checked>
       </div>
     `;
     })
@@ -136,19 +144,18 @@ const fetchBrands = async () => {
 const renderBrands = async() => {
   const brandsname = await fetchBrands();
   spanNbBrands.innerHTML = brandsname.length;
-  brandsname.unshift('all');
-  const options = Array.from(brandsname
-    .map(brand => `<option value="${brand}">${brand}</option>`)
+  brandsname.unshift('All');
+  const options = Array.from(brandsname,
+    (brand => `<option value="${brand}">${brand}</option>`)
   ).join('');
     
   selectBrand.innerHTML = options;
 };
-  
+
 const render = (products, pagination) => {
   renderProducts(products);
   renderPagination(pagination);
   renderIndicators(pagination);
-  renderBrands(products);
 };
 
 /**
@@ -169,12 +176,7 @@ selectShow.addEventListener('change', async (event) => {
   render(currentProducts, currentPagination);
 });
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const products = await fetchProducts();
-  
-  setCurrentProducts(products);
-  render(currentProducts, currentPagination);
-});
+
 
 /**Feature 1 - Browse pages
 As a user
@@ -208,62 +210,194 @@ selectBrand.addEventListener('change', async (event) => {
 As a user
 I want to filter by by recent products
 So that I can browse the new released products (less than 2 weeks) */
+selectReleased.addEventListener('click', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  const today = new Date();
+  const twoWeeksAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 14);
+  products.result = products.result.filter(product => {
+    const date = new Date(product.released);
+    return date > twoWeeksAgo;
+  });
 
-/** filter by recent products */
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
 
 /**
 Feature 4 - Filter by reasonable price
 As a user
 I want to filter by reasonable price
 So that I can buy affordable product i.e less than 50€ */
+selectPrice.addEventListener('click', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  products.result = products.result.filter(product => product.price < 50);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
 /** 
 Feature 5 - Sort by price
 As a user
 I want to sort by price
-So that I can easily identify cheapest and expensive products */
-/** 
+So that I can easily identify cheapest and expensive products 
+
+AND
+
 Feature 6 - Sort by date
 As a user
 I want to sort by price
 So that I can easily identify recent and old products */
+selectSort.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  if(event.target.value == 'price-asc'){
+    products.result.sort((a, b) => a.price - b.price);
+  } else if(event.target.value == 'price-desc'){
+    products.result.sort((a, b) => b.price - a.price);
+  }
+  else if(event.target.value == 'date-asc'){
+    products.result.sort((a, b) => new Date(a.released) - new Date(b.released));
+  } else if(event.target.value == 'date-desc'){
+    products.result.sort((a, b) => new Date(b.released) - new Date(a.released));
+  }
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
 /**
 Feature 8 - Number of products indicator
 As a user
 I want to indicate the total number of products
 So that I can understand how many products is available */
-
+/** It's alraedy done in renderIndicator 
+ * const renderIndicators = pagination => {
+  const {count} = pagination;
+  spanNbProducts.innerHTML = count;
+};
+*/
 /**
 Feature 9 - Number of recent products indicator
 As a user
 I want to indicate the total number of recent products
 So that I can understand how many new products are available */
+function getRecentProducts(products){
+  const today = new Date();
+  const twoWeeksAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 14);
+  const recentProducts = products.filter(product => {
+    const date = new Date(product.released);
+    return date > twoWeeksAgo;
+  });
+  return recentProducts.length;
+}
+
+const renderNewProductsIndicator = async() => {
+  const products = await fetchProducts(1,currentPagination.count);
+  const recentProducts = getRecentProducts(products.result);
+  spanNbNewProducts.innerHTML = recentProducts;
+};
+
 /**
 Feature 10 - p50, p90 and p95 price value indicator
 As a user
 I want to indicate the p50, p90 and p95 price value
 So that I can understand the price values of the products */
+const quantile = (arr, q,attribute) => {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const pos = (sorted.length-1) * q;
+  const base = Math.floor(pos);
+  const rest = pos - base;
+  if ((sorted[base + 1][attribute] !== undefined)) {
+    return Math.round(sorted[base][attribute] + rest * (sorted[base + 1][attribute] - sorted[base][attribute]));
+  } else {
+    return Math.round(sorted[base][attribute]);
+  }
+};
+
 /**
 Feature 11 - Last released date indicator
 As a user
 I want to indicate the last released date
 So that I can understand if we have new products */
+const getLastReleasedDate = products => {
+  const sorted = products.slice().sort((a, b) => {
+    const dateA = new Date(a.released);
+    const dateB = new Date(b.released);
+    return dateB - dateA;
+  });
+  return sorted[0].released;
+};
+
 /**
 Feature 12 - Open product link
 As a user
 I want to open product link in a new page
-So that I can buy the product easily */
+So that I can buy the product easily 
+
+We just have to add target="_blank" in the html
+*/
+
 /**
 Feature 13 - Save as favorite
 As a user
 I want to save a product as favorite
 So that I can retreive this product later */
+const saveFavorite = async (id) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  const product = products.result.find(product => product.id == id);
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  favorites.push(product);
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+};
+
+function setupFavoriteButton() {
+  const buttons = document.querySelectorAll('.star');
+  buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const id = event.target.dataset.id;
+      saveFavorite(id);
+    });
+  });
+}
+
 /**
 Feature 14 - Filter by favorite
 As a user
 I want to filter by favorite products
 So that I can load only my favorite products */
+/*
+selectFavorite.addEventListener('click', async (event) => {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  const products = await fetchProducts(1,currentPagination.count);
+  products.result = products.result.filter(product => favorites.find(favorite => favorite.id == product.id));
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});*/
 /**
 Feature 15 - Usable and pleasant UX
 As a user
 I want to parse a usable and pleasant web page
-So that I can find valuable and useful content */
+So that I can find valuable and useful content 
+
+We just have to add some css into the html
+*/
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const products = await fetchProducts();
+  setCurrentProducts(products);
+  const allProducts = await fetchProducts(1, currentPagination.count);
+
+  const brands = await fetchBrands();
+  renderBrands(brands);
+  renderNewProductsIndicator(allProducts);
+  render(currentProducts, currentPagination);
+  const p50 = quantile(allProducts.result, 0.5, "price");
+  const p90 = quantile(allProducts.result, 0.90, "price");
+  const p95 = quantile(allProducts.result, 0.95, "price");
+  document.getElementById('p50').innerHTML = Math.round(p50*100)/100;
+  document.getElementById('p90').innerHTML = Math.round(p90*100)/100;
+  document.getElementById('p95').innerHTML = Math.round(p95*100)/100;
+  document.getElementById('lastReleasedDate').innerHTML = getLastReleasedDate(allProducts.result);
+  setupFavoriteButton();
+});
